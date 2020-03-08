75a88fb5a32ff8defbae3c49848ad87738469282
bunnida <bunnida.tw@gmail.com>
2020-03-09 00:08:49 +0800

    Organize exit-related variables and functions

--- fib_valid.py
+++ fib_valid.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import sys
-import urllib.request
 
 
 EXIT_CODE_SUCCESS = 0
 EXIT_CODE_FAIL    = 1
+def _ExitError(Msg=None):
+    if Msg: print(Msg)
+    sys.exit(EXIT_CODE_FAIL)
+def _ExitNormal(Msg=None):
+    if Msg: print(Msg)
+    sys.exit(EXIT_CODE_SUCCESS)
 
 
 def _ExtractToken(Source, BeginString, EndString):
@@ -13,11 +18,6 @@ def _ExtractToken(Source, BeginString, EndString):
     q = Source.find(EndString, p + 1)
 
     return '-1' if p < 0 or q < 0 else Source[p:q]
-
-
-def _ExitError(Msg=None):
-    if Msg: print(Msg)
-    sys.exit(EXIT_CODE_FAIL)
 
 
 def main():
@@ -32,7 +32,8 @@ def main():
     url = BASE_URL.format(FIB_NUM_INDEX)
 
     try:
-        html = str(urllib.request.urlopen(url).read())
+        import urllib.request as url_req
+        html = str(url_req.urlopen(url).read())
     except BaseException as E:
         _ExitError("Could not access web page: '{}'".format(E))
 
@@ -40,7 +41,7 @@ def main():
         fib_num = _ExtractToken(html, BASE_DICT[base], BASE_END)
         print('{} {}(base:{})'.format(FIB_NUM_INDEX, fib_num, base))
 
-    sys.exit(EXIT_CODE_SUCCESS)
+    _ExitNormal()
 
 
 if __name__ == '__main__':
