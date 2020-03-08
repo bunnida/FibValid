7e9f0f2a92375c2e8fe9d68a29feccbfd1edd4fa
bunnida <bunnida.tw@gmail.com>
2020-03-09 00:04:31 +0800

    Move global variables into functions

--- fib_valid.py
+++ fib_valid.py
@@ -4,39 +4,37 @@ import sys
 import urllib.request
 
 
-FIB_NUM_INDEX = -1
-BASE_URL = 'http://www.protocol5.com/Fibonacci/{}.htm'
-
 EXIT_CODE_SUCCESS = 0
 EXIT_CODE_FAIL    = 1
 
-BASE_DICT = {
-      2: 'Base 2:</h4><div>'   ,
-     10: 'Base 10:</h4><div>'  ,
-     16: 'Radix 16:</h4><div>' }
-BASE_END = '</div>'
 
 def _ExtractToken(Source, BeginString, EndString):
     p = Source.find(BeginString) + len(BeginString)
     q = Source.find(EndString, p + 1)
 
-    token = Source[p:q]
+    return '-1' if p < 0 or q < 0 else Source[p:q]
 
-    return token
 
-def _PrintErrorMessageAndExit(Msg):
-    print(Msg)
+def _ExitError(Msg=None):
+    if Msg: print(Msg)
     sys.exit(EXIT_CODE_FAIL)
 
 
 def main():
     FIB_NUM_INDEX = '300'
+    BASE_DICT = {
+         2: 'Base 2:</h4><div>'   ,
+        10: 'Base 10:</h4><div>'  ,
+        16: 'Radix 16:</h4><div>' }
+    BASE_END = '</div>'
+    BASE_URL = 'http://www.protocol5.com/Fibonacci/{}.htm'
+
     url = BASE_URL.format(FIB_NUM_INDEX)
 
     try:
         html = str(urllib.request.urlopen(url).read())
     except BaseException as E:
-        _PrintErrorMessageAndExit("Could not access web page: '{}'".format(E))
+        _ExitError("Could not access web page: '{}'".format(E))
 
     for base in sorted(BASE_DICT.keys()):
         fib_num = _ExtractToken(html, BASE_DICT[base], BASE_END)
