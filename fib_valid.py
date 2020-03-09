d680b13f5ffcea5f57f390c27fa6be3580be4285
bunnida <bunnida.tw@gmail.com>
2020-03-09 18:06:36 +0800

    Add more exiting conditional code

--- fib_valid.py
+++ fib_valid.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python3
 
-
-EXIT_CODE_SUCCESS, EXIT_CODE_FAIL = 0, 1
-def _Exit(Msg=None, ErrCode=None):
+EXIT_PROG_FAIL, EXIT_ANSWER_INCORRECT, EXIT_ANSWER_CORRECT = 2, 1, 0
+def _Exit(ErrCode, Msg=None):
     if Msg: print(Msg)
 
     import sys
-    sys.exit(EXIT_CODE_FAIL if ErrCode else EXIT_CODE_SUCCESS)
-
+    sys.exit(ErrCode)
 
 def _LoadWebPageAndExtractBaseNumberPair(FibNumberIndex):
-    BASE_URL = 'http://www.protocol5.com/Fibonacci/{}.htm'
-    url = BASE_URL.format(FibNumberIndex)
-    try:
-        import urllib.request as url_req
-        html = url_req.urlopen(url).read().decode('utf-8')
-    except BaseException as E:
-        _Exit("Could not access web page: '{}'".format(E), EXIT_CODE_FAIL)
+    url = 'http://www.protocol5.com/Fibonacci/{}.htm'.format(FibNumberIndex)
+
+    import urllib.request as url_req
+    try: html = url_req.urlopen(url).read().decode('utf-8')
+    except BaseException as E: _Exit(EXIT_PROG_FAIL, "Could not access web page: '{}'".format(E))
 
     pairs = []
     import re
-    first_screen = re.findall('<li><h4>.*?</li>', html)
-    for line in first_screen:
-        base, num = re.findall(' ([0-9]*):.*v>(.*)</d', line)[0]
-        pairs.append([base, num])
+    for line in re.findall('<li><h4>.*?</li>', html):
+        pairs.append(  re.findall(' ([0-9]*):.*v>(.*)</d', line)[0]  )
 
     return pairs
 
-
 def main():
-    pairs = _LoadWebPageAndExtractBaseNumberPair(999001)
+    pairs = _LoadWebPageAndExtractBaseNumberPair(300)
     for p in pairs: print(p)
 
-    _Exit()
-
+    _Exit(EXIT_ANSWER_CORRECT)
 
 if __name__ == '__main__':
     main()
